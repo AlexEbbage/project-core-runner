75427b92d2ca87d0d027333d354ccc0b17c9afd2
Alex Ebbage <Alexebbage@gmail.com>
2026-01-14 22:32:02 +0000

    Avoid overriding non-touch input in button mode

--- Assets/Scripts/Settings/SettingsData.cs
+++ Assets/Scripts/Settings/SettingsData.cs
@@ -6,6 +6,13 @@ public static class SettingsData
     private const string MusicVolumeKey = "settings_music_volume";
     private const string SfxVolumeKey = "settings_sfx_volume";
     private const string VibrateKey = "settings_vibrate";
+    private const string TouchInputModeKey = "settings_touch_input_mode";
+
+    public enum TouchInputMode
+    {
+        Drag = 0,
+        Buttons = 1
+    }
 
     public static float MusicVolume
     {
@@ -23,5 +30,21 @@ public static class SettingsData
     {
         get => PlayerPrefs.GetInt(VibrateKey, 1) == 1;
         set { PlayerPrefs.SetInt(VibrateKey, value ? 1 : 0); PlayerPrefs.Save(); }
+    }
+
+    public static TouchInputMode CurrentTouchInputMode
+    {
+        get
+        {
+            int saved = PlayerPrefs.GetInt(TouchInputModeKey, (int)TouchInputMode.Drag);
+            if (!System.Enum.IsDefined(typeof(TouchInputMode), saved))
+                saved = (int)TouchInputMode.Drag;
+            return (TouchInputMode)saved;
+        }
+        set
+        {
+            PlayerPrefs.SetInt(TouchInputModeKey, (int)value);
+            PlayerPrefs.Save();
+        }
     }
 }
